f5a92b8b94623b1705f42de81cb4d9783e5cbc5c
CAMIANAIS <cmamanicr@unsa.edu.pe>
2023-10-27 17:10:14 -0500

    included HU1 corrected test just added structure o observable

--- burger-queen-api-client/src/app/components/login/login.component.spec.ts
+++ burger-queen-api-client/src/app/components/login/login.component.spec.ts
@@ -1,51 +1,77 @@
 import { TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture } from '@angular/core/testing';
 import { LoginService } from 'src/app/services/login.service';
-import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from 'src/app/shared/components/header.component';
-import { MatCard,MatCardContent } from '@angular/material/card';
-import { MatFormField } from '@angular/material/form-field';
-import { MatError } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { of } from 'rxjs';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  const loginServiceSpy = jasmine.createSpyObj('LoginService', ['login']);
   beforeEach(() => {
-   /*const httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);*/
-  
-  TestBed.configureTestingModule({
-      declarations: [LoginComponent,HeaderComponent,MatCard,MatCardContent,MatFormField,MatError],
-      imports: [HttpClientTestingModule,ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatButtonModule, FormsModule,BrowserAnimationsModule],
-      providers: [LoginService], 
-      
-    });
+    loginServiceSpy.login.calls.reset();
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    const httpClientSpy = jasmine.createSpyObj('HttpClient', ['post']);
+    TestBed.configureTestingModule({
+      declarations: [LoginComponent, HeaderComponent],
+      imports: [
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+      ],
+      providers: [
+        FormBuilder,
+        { provide: LoginService, useValue: loginServiceSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: HttpClient, useValue: httpClientSpy },
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges(); 
-    });
+    fixture.detectChanges();
+  });
 
-    it('initialize the form', () => {
-      expect(component.loginForm).toBeTruthy();
-      expect(component.loginForm.get('email')).toBeTruthy();
-      expect(component.loginForm.get('password')).toBeTruthy();
-});
-    it('should call login when the form is valid', () => {
-      const credentials = {
+
+  it('initialize the form', () => {
+    expect(component.loginForm).toBeTruthy();
+    expect(component.emailData).toBeTruthy();
+    expect(component.passwordData).toBeTruthy();
+  });
+
+  it('should call login when the form is valid', () => {
+    const credentials = {
+      email: 'test@example.com',
+      password: 'myRealPassword',
+    };
+    component.loginForm.setValue(credentials);
+
+    // Set up the behavior for this specific test
+    loginServiceSpy.login.and.returnValue(of({
+      accessToken: 'yourAccessTokenHere',
+      user: {
         email: 'test@example.com',
-        password: 'myRealPassword',
-      };
-      component.loginForm.setValue(credentials);
-      const loginService = TestBed.inject(LoginService);
-      spyOn(loginService,'login');
+        role: 'userRoleHere',
+        id: 'userIdHere'
+      }
+    }));
+    component.save(new Event('click'));
 
-      component.save(new Event('click')); 
-      expect(loginService.login).toHaveBeenCalledWith(credentials.email, credentials.password);
-      });
-
+    expect(loginServiceSpy.login).toHaveBeenCalledWith(credentials.email, credentials.password);
+  });
 });
--- burger-queen-api-client/src/app/components/login/login.component.ts
+++ burger-queen-api-client/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup,Validators } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
 import { Router } from '@angular/router';
 
@@ -13,17 +13,37 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   errorMessage: string = '';
-
+  attrsToShowPassword = {
+    inputPasswordType: 'password',
+    iconClass: 'far fa-eye'
+  };
 
   constructor(private formBuilder: FormBuilder, private apiService: LoginService, private router:Router) {
     this.loginForm = this.formBuilder.group({
-      email: '', 
-      password: '', 
+      email: ["", [Validators.required, Validators.pattern('[A-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      password: ["", Validators.required]
     });
     console.log(this.apiService)
   }
 
+  isFieldInvalid(fieldName: string) {
+    return (
+      this.loginForm?.get(fieldName)?.invalid &&
+      this.loginForm.get(fieldName)?.touched
+    );
+  }
 
+  showPsw() {
+    const passwordViewOps = {
+      inputPasswordType: 'text',
+      iconClass: 'far fa-eye-slash'
+   };
+   const passwordOps = {
+      inputPasswordType: 'password',
+      iconClass: 'far fa-eye'
+    }
+   this.attrsToShowPassword = this.attrsToShowPassword.inputPasswordType == "password" ? passwordViewOps : passwordOps;
+  }
 
   ngOnInit() {}
 
